9f3df936306ff5201feeaeaf6cdffe7c01ddcb1f
Daniel Quintero <dag@DanielSB.localdomain>
2019-03-12 00:18:01 -0400

    end of sprint1

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //This file holds any configuration variables we may need 
 //'config.js' is typically ignored by git to protect sensitive information, such as your database's username and password
 
