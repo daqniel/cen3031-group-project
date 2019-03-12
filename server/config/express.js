02995508559c419b1d538951300145af61c2ce2f
leingad <danqmail@gmail.com>
2019-03-11 21:03:33 -0400

    notes and blogposts basic functionality

--- server/config/express.js
+++ server/config/express.js
@@ -6,7 +6,11 @@ var path = require("path"),
   //bodyParser = require('body-parser'),
   config = require("./config"),
   specialsRouter = require("../routes/special.routes"),
-  usersRouter = require("../routes/user.routes");
+  usersRouter = require("../routes/special.routes"),
+  // requestsRouter = require("../routes/request.routes"),
+  // recommendationsRouter = require("../routes/recommendation.routes"),
+  notesRouter = require("../routes/note.routes"),
+  blogpostsRouter = require("../routes/blogpost.routes");
 
 module.exports.init = function() {
   /* db connect */
@@ -27,6 +31,10 @@ module.exports.init = function() {
   // Use the listings router for requests to the api */
   app.use("/api/users", usersRouter);
   app.use("/api/specials", specialsRouter);
+  // app.use("/api/requests", requestsRouter);
+  // app.use("/api/recommendations", recommendationsRouter);
+  app.use("/api/notes", notesRouter);
+  app.use("/api/blogposts", blogpostsRouter);
 
   // Go to homepage for all routes not specified */
   app.get("*", function(req, res) {
