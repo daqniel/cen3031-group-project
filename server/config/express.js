f51a2c8ffa9190500ffbe928e68132f438fca4de
Daniel Quintero <dag@DanielSB.localdomain>
2019-03-12 00:14:03 -0400

    end of sprint 1

--- server/config/express.js
+++ server/config/express.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var path = require("path"),
   express = require("express"),
   mongoose = require("mongoose"),
@@ -43,44 +42,4 @@ module.exports.init = function() {
   });
   return app;
 };
-=======
-var path = require('path'),
-    express = require('express'),
-    mongoose = require('mongoose'),
-    morgan = require('morgan'),
-    bodyParser = require('body-parser'),
-    config = require('./config'),
-    listingsRouter = require('../routes/listings.server.routes');
 
-module.exports.init = function() {
-  //connect to database
-  mongoose.connect(config.db.uri);
-
-  //initialize app
-  var app = express();
-
-  //enable request logging for development debugging
-  app.use(morgan('dev'));
-
-  //body parsing middleware
-  app.use(bodyParser.json());
-
-
-  /**TODO
-  Serve static files */
-    app.use(express.static('client'));
-
-
-  /**TODO
-  Use the listings router for requests to the api */
-    app.use('/api/listings', listingsRouter);
-
-
-  /**TODO
-  Go to homepage for all routes not specified */
-    app.get('*', function(req, res){
-        res.redirect('/');
-    });
-  return app;
-};
->>>>>>> master
