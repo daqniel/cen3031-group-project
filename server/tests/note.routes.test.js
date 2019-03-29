1e64c9699ef99fbdb51870c45f68b8e27364193f
jackfining <32111237+jackfining@users.noreply.github.com>
2019-03-28 22:58:39 -0400

    Updated mocha test file for Note model

--- server/tests/note.routes.test.js
+++ server/tests/note.routes.test.js
@@ -10,7 +10,7 @@ var note = { //new Note created to test save, update, and delete calls
     type: 'Client',
     linkedId: '5c990e800e74a10b93c91caf',
     title: 'Also has fear of rivers.',
-    text: 'Also avoid recommending everything near rivers'
+    text: 'Also avoid recommending everything near rivers.'
 };
 
 var id; //ObjectId of created test Note
@@ -34,7 +34,7 @@ describe('Tests for Note API call', function() {
                should.exist(res.body._id);
                res.body.linkedId.should.equal('5c990e800e74a10b93c91caf');
                res.body.type.should.equal('Client');
-               res.body.text.should.equal('Also has fear of rivers.');
+               res.body.title.should.equal('Also has fear of rivers.');
                res.body.text.should.equal('Also avoid recommending everything near rivers.');
                id = res.body._id;
                done();
@@ -42,50 +42,38 @@ describe('Tests for Note API call', function() {
     });
 
     it('Get Note by _id', function(done) {
-        if(err){
-            console.log(err);
-        }else{
-            agent.get('/api/notes/5c9912f2ad71b60bab26e7b5') //specifies test note's ObjectId
-                .expect(200)
-                .end(function (err, res) {
-                    should.not.exist(err);
-                    should.exist(res);
-                    res.body.linkedId.should.equal('5c990f6f0e74a10b93c91cb1');
-                    res.body.type.should.equal('Client');
-                    res.body.title.should.equal('This guy failed me three times.');
-                    res.body.text.should.equal('Recommend him a one way trip to Siberia.');
-                    res.body._id.should.equal('5c9912f2ad71b60bab26e7b5');
-                    done();
-                });
-        }
+        agent.get('/api/notes/' + id) //specifies test note's ObjectId
+            .expect(200)
+            .end(function (err, res) {
+                should.not.exist(err);
+                should.exist(res);
+                res.body.linkedId.should.equal('5c990e800e74a10b93c91caf');
+                res.body.type.should.equal('Client');
+                res.body.title.should.equal('Also has fear of rivers.');
+                res.body.text.should.equal('Also avoid recommending everything near rivers.');
+                res.body._id.should.equal(id);
+                done();
+            });
     });
 
     it('Get all Notes', function(done) {
-        if(err){
-            console.log(err);
-        }else{
-            agent.get('/api/notes')
-                .expect(200)
-                .end(function (err, res) {
-                    should.not.exist(err);
-                    should.exist(res);
-                    done();
-                });
-        }
+        agent.get('/api/notes')
+            .expect(200)
+            .end(function (err, res) {
+                should.not.exist(err);
+                should.exist(res);
+                done();
+            });
     });
 
     it('Get all Notes by linkedId', function(done) {
-        if(err){
-            console.log(err);
-        }else{
-            agent.get('/api/notes/?linkedId=5c990f6f0e74a10b93c91cb1') //specifies a linkedId shared by multiple test Notes
-                .expect(200)
-                .end(function (err, res) {
-                    should.not.exist(err);
-                    should.exist(res);
-                    done();
-                });
-        }
+        agent.get('/api/notes/?linkedId=5c990e800e74a10b93c91caf') //specifies a linkedId shared by multiple test Notes
+            .expect(200)
+            .end(function (err, res) {
+                should.not.exist(err);
+                should.exist(res);
+                done();
+            });
     });
 
     it('Update Note by _id', function(done) {
@@ -95,7 +83,7 @@ describe('Tests for Note API call', function() {
             title: 'Also has fear of rivers, streams, and lakes.',
             text: 'Also avoid recommending everything near rivers or fresh water.'
         };
-        agent.put('/api/notes/5c9912f2ad71b60bab26e7b5')
+        agent.put('/api/notes/' + id)
             .send(noteUpdate)
             .expect(200)
             .end(function(err, res){
@@ -103,12 +91,13 @@ describe('Tests for Note API call', function() {
                 should.exist(res);
                 res.body.linkedId.should.equal('5c990e800e74a10b93c91caf');
                 res.body.type.should.equal('Client');
-                res.body.text.should.equal('Also has fear of rivers, streams, and lakes.');
+                res.body.title.should.equal('Also has fear of rivers, streams, and lakes.');
                 res.body.text.should.equal('Also avoid recommending everything near rivers or fresh water.');
                 res.body._id.should.equal(id);
                 done();
             });
     });
+
     // it('Delete Note by _id', function(done) {
     //
     // });
