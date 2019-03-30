40a29b075530c3c8b3c729daf2b88dbab0632d40
swalker15 <sarah.walker@ufl.edu>
2019-03-30 17:50:11 -0400

    BlogPost, User client controllers and factories added, tested in index

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,7 @@
 /* register the modules the application depends upon here*/
 angular.module('specials', []);
+angular.module('blogPosts',[]);
+angular.module('users',[]);
 
 /* register the application and inject all the necessary dependencies */
-var app = angular.module('agencyApp', ['specials']);
+var app = angular.module('agencyApp', ['specials','blogPosts','users']);
