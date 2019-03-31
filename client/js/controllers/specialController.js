7272874b64b1c0df2bb89f626c78e02acf668789
Dan <daniel@Daniels-MacBook-Pro-2.local>
2019-03-31 14:43:57 -0400

    updated special controller

--- client/js/controllers/specialController.js
+++ client/js/controllers/specialController.js
@@ -1,47 +1,36 @@
-angular.module('specials').controller('SpecialsController', ['$scope', 'Specials', 
-  function($scope, Specials) {
+angular.module('specials').controller('SpecialsController', ['$scope', 'Specials',
+  function ($scope, Specials) {
     /* Get all the listings, then bind it to the scope */
-    Specials.get3MostRecent().then(function(response) {
-      $scope.specials = response.data;
-      $scope.special1 = response.data[0];
-      $scope.special2 = response.data[1];
-      $scope.special3 = response.data[2];
-    }, function(error) {
-      console.log('Unable to retrieve specials:', error);
-    });
-    
-
-    $scope.addSpecial = function(list) {
-	
-      Specials.create(list).then(function(response) {
-      window.location=window.location;
-    }, function(error) {
-      console.log('Unable to retrieve specials:', error);
-    });
-
-
-    };
-
-    $scope.deleteSpecial = function(id) {
-      Specials.delete(id).then(function(response)
-      {
+    $scope.get3MostRecent = function () {
+      Specials.get3MostRecent().then(function (response) {
         $scope.specials = response.data;
-          
-          Specials.getAll().then(function(response) {
-            $scope.specials = response.data;
-          }, function(error) {
-            console.log('Unable to retrieve specials:', error);
-          });}, function(error) {
+        $scope.special1 = response.data[0];
+        $scope.special2 = response.data[1];
+        $scope.special3 = response.data[2];
+      }, function (error) {
         console.log('Unable to retrieve specials:', error);
       });
-
-
     };
 
-    // $scope.showDetails = function(index) {
-    //   $scope.special1 = specials[0];
-    //   $scope.special2 = specials[1];
-    //   $scope.special3 = specials[2];
-    // };
+    $scope.addSpecial = function (list) {
+      Specials.create(list).then(function (response) {
+        window.location = window.location;
+      }, function (error) {
+        console.log('Unable to retrieve specials:', error);
+      });
+    };
+
+    $scope.deleteSpecial = function (id) {
+      Specials.delete(id).then(function (response) {
+        $scope.specials = response.data;
+        Specials.getAll().then(function (response) {
+          $scope.specials = response.data;
+        }, function (error) {
+          console.log('Unable to retrieve specials:', error);
+        });
+      }, function (error) {
+        console.log('Unable to retrieve specials:', error);
+      });
+    };
   }
 ]);
