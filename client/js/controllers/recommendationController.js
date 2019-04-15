c7f8b43c6f0a0c4745f352cfbe1779c02bd5bee5
Daniel <Daagniel@gmail.com>
2019-04-15 17:37:58 -0400

    refactoring, needs work

--- client/js/controllers/recommendationController.js
+++ client/js/controllers/recommendationController.js
@@ -1,36 +1,48 @@
-angular.module('recommendation').controller('RecommendationsController', ['$scope', 'Recommendations', 
+//TODO: bring this up to par with other controllers
+angular.module("recommendation").controller("RecommendationsController", [
+  "$scope",
+  "Recommendations",
   function($scope, Recommendations) {
-    Recommendations.getAll().then(function(response) {
-      $scope.recommendations = response.data;
-    }, function(error) {
-      console.log('Unable to retrieve recommendations:', error);
-    });
+    Recommendations.getAll().then(
+      function(res) {
+        $scope.recommendations = res.data;
+      },
+      function(error) {
+        console.log("Unable to retrieve recommendations:", error);
+      }
+    );
     $scope.detailedInfo = undefined;
 
+    /* this is going to have to change after we settle on fields for schema */
     $scope.addRecommendation = function(rec) {
-      Recommendations.create(rec).then(function(response) {
-      window.location=window.location;
-    }, function(error) {
-      console.log('Unable to retrieve recommendations:', error);
-    });
-
+      Recommendations.create(rec).then(
+        function(res) {
+          window.location = window.location;
+        },
+        function(error) {
+          console.log("Unable to retrieve recommendations:", error);
+        }
+      );
     };
 
     $scope.deleteRecommendations = function(id) {
-	
-      Recommendations.delete(id).then(function(response)
-      {
-        $scope.recommendations = response.data;
-          
-        Recommendations.getAll().then(function(response) {
-            $scope.recommendations = response.data;
-          }, function(error) {
-            console.log('Unable to retrieve recommendations:', error);
-          });}, function(error) {
-        console.log('Unable to retrieve recommendations:', error);
-      });
+      Recommendations.delete(id).then(
+        function(res) {
+          $scope.recommendations = res.data;
 
-
+          Recommendations.getAll().then(
+            function(res) {
+              $scope.recommendations = res.data;
+            },
+            function(error) {
+              console.log("Unable to retrieve recommendations:", error);
+            }
+          );
+        },
+        function(error) {
+          console.log("Unable to retrieve recommendations:", error);
+        }
+      );
     };
 
     $scope.showDetails = function(index) {
