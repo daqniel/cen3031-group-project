07b7cbf9f675a4e4823a6580fd1b81ce526bdbbb
swalker15 <sarah.walker@ufl.edu>
2019-04-23 07:33:44 -0400

    merge

--- client/js/controllers/userController.js
+++ client/js/controllers/userController.js
@@ -4,7 +4,6 @@ angular.module("users").controller("UsersController", [
   function($scope, Users) {
     console.log("this ran");
     Users.getSession().then(response => {
-      $scope.currentUser = null;
       console.log("getting session", response);
       // $scope.currentUser = $.parseJSON(sessionStorage.getItem("user"));
       if (response.status == 200) {
@@ -12,15 +11,11 @@ angular.module("users").controller("UsersController", [
           $scope.loggedIn = "no";
         } else if (response.data.isAdmin == true) {
           $scope.loggedIn = "admin";
-          $scope.currentUser = response.data.user;
-          $scope.sessionUsername = $scope.currentUser;
+          $scope.sessionUsername = response.data.user;
         } else {
           $scope.loggedIn = "yes";
-          $scope.currentUser = response.data.user;
-          $scope.sessionUsername = $scope.currentUser;
+          $scope.sessionUsername = response.data.user;
         }
-        // console.log("test", response.text, response.body.text);
-        // console.log("all good, got session");
       }
 
       /* Get all the listings, then bind it to the scope */
@@ -60,48 +55,13 @@ angular.module("users").controller("UsersController", [
             window.location.href = "/home";
           });
       };
-      $scope.updateUser = function(id,
-        updatedFirst,
-        updatedMiddle,
-        updatedLast,
-        updatedEmail,
-        updatedPhone,
-        updatedPass
-        ) {
-          var updatedUser = {
-            name: {
-              first: updatedFirst,
-              middle: updatedMiddle,
-              last: updatedLast
-            },
-            email: updatedEmail,
-            password: updatedPass,
-            phoneNumber: updatedPhone
-          };
-          Users.update(id, updatedUser)
-            .then(res => {
-              window.location.href = "#!/home";
-              console.log("user successfully updated:", res.data);
-            })
-            .catch(err => {
-              console.log("couldn't update user: ", err);
-            })
-        }
 
       $scope.authenticateUser = function(email, password) {
         Users.authenticate(email, password)
           .then(res => {
             console.log("res.data", res);
             if (res.status == 200) {
-              // if (res.data.email.substr(0, 6) == "BTADM-") {
-              //   sessionStorage.setItem("user", JSON.stringify(res.data));
-              //   $scope.loggedIn = "admin";
-              //   window.location.href = "/home";
-              // } else {
-                 sessionStorage.setItem("user", JSON.stringify(res.data));
-              //   $scope.loggedin = "yes";
-              //   window.location.href = "/home";
-              // }
+              sessionStorage.setItem("user", JSON.stringify(res.data));
               console.log("looged in successfully");
               window.location.href = "/home";
             }
@@ -117,12 +77,9 @@ angular.module("users").controller("UsersController", [
           .then(response => {
             console.log(response.status);
             if (response.status == 200) {
-              console.log("the heck");
               $scope.loggedIn = "no";
-              // sessionStorage.removeItem("user");
+              sessionStorage.removeItem("user");
               window.location.href = "/home";
-            } else {
-              console.log("wthell");
             }
           })
           .catch(err => {
@@ -130,14 +87,8 @@ angular.module("users").controller("UsersController", [
           });
       };
 
-      $scope.getSession = function() {
-        console.log("SHOULD NOT BE RUNNING");
-
-        Users.getSession().then(response => {
-          $scope.currentUser = $.parseJSON(sessionStorage.getItem("user"));
-          if (response.status == 200) {
-          }
-        });
+      $scope.getUserInfo = function() {
+        $scope.currentUser = $.parseJSON(sessionStorage.getItem("user"));
       };
 
       $scope.deleteUser = function(id) {
@@ -164,14 +115,6 @@ angular.module("users").controller("UsersController", [
       $scope.showDetails = function(index) {
         $scope.detailedInfo = $scope.listings[index];
       };
-
-      // $scope.showName = function() {
-      //   // $scope.sessionUsername = $.parseJSON(
-      //     // sessionStorage.getItem("user")
-      //   // ).email;
-      //   $scope.sessionUsername = $scope.currentUser.user;
-      //   console.log("sessionname", $scope.sessionUsername);
-      // };
     });
   }
 ]);
